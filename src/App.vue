8227895396bfd0621d5288f04a02584bb60edd1a
inst.abhishek <inst.abhishek@gmail.com>
2024-06-14 12:40:59 +0000

    Feature:total balance calculation with computed()

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
   <div class="container">
 
-    <Balance />
+    <Balance :total="total" />
     <IncomeExpenses />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
@@ -13,17 +13,26 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
+// List of transactions
 const transactions = ref([
   { id: 1, text: 'Flower', amount: -20, },
   { id: 2, text: 'Income', amount: 12, },
   { id: 3, text: 'Tickets', amount: -500, },
+  { id: 4, text: 'Salary', amount: 652, },
 ]);
+
+// Total Balance
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
 
 </script>
