c1165e50e164b61f017593a41f3f18f89a6dfc2b
inst.abhishek <inst.abhishek@gmail.com>
2024-06-14 12:28:51 +0000

    Transactions data, props added

--- src/App.vue
+++ src/App.vue
@@ -5,30 +5,25 @@
 
     <Balance />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
 
   </div>
 
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue';
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction
-  }
-}
-
-
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -20, },
+  { id: 2, text: 'Income', amount: 12, },
+  { id: 3, text: 'Tickets', amount: -500, },
+]);
 
 </script>
