8e7aa8e7cc7349be1157dff06ea0b51844eb7d9b
inst.abhishek <inst.abhishek@gmail.com>
2024-06-14 12:39:33 +0000

    fix: remove - sign

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -3,7 +3,7 @@
   <ul id="list" class="list">
 
     <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
-      {{ transaction.text }} <span>-$ {{ transaction.amount }}</span><button class="delete-btn">x</button>
+      {{ transaction.text }} <span>$ {{ transaction.amount }}</span><button class="delete-btn">x</button>
     </li>
 
   </ul>
